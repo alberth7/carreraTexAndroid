73ae656466905ec05ce5ef4dfe507744ce6f59cc
alberth7 <maikeru.bash@gmail.com>
2018-10-15 23:38:02 -0400

    fallo degradado

---
+++ app/src/main/java/com/example/alberth/carreracochestex/Acelerometro.java
@@ -0,0 +1,13 @@
+package com.example.alberth.carreracochestex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Acelerometro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acelerometro);
+    }
+}
---
+++ app/src/main/java/com/example/alberth/carreracochestex/Menu.java
@@ -0,0 +1,30 @@
+package com.example.alberth.carreracochestex;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Menu extends AppCompatActivity {
+
+    //TextView titulo;
+    //Typeface habes;
+    Button btntouch,btnAcelerometro,btnCreditos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+   // btntouch=(Button)findViewById(R.id.BtnTouch);
+
+    //titulo=(TextView)findViewById(R.id.TxtTitulo);
+   // titulo.setTypeface(habes);
+
+
+
+
+    }
+}
