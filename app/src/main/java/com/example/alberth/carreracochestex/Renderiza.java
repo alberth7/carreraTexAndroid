5f5f95072ab6e568ecc89c329a347e0840721afe
alberth7 <maikeru.bash@gmail.com>
2018-10-15 21:30:26 -0400

    dos coches

--- app/src/main/java/com/example/alberth/carreracochestex/Renderiza.java
+++ app/src/main/java/com/example/alberth/carreracochestex/Renderiza.java
@@ -5,9 +5,7 @@ import javax.microedition.khronos.opengles.GL10;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
-import android.opengl.GLSurfaceView.Renderer;
 import android.view.MotionEvent;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -18,7 +16,9 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 	private Textura texturaCarretera;
 	private Textura texturaCoche1;
 	private Textura texturaCoche2;
-	
+	private Textura texturaCoche3;
+
+
 	/* Contexto */
 	private Context contexto;
 	
@@ -29,10 +29,21 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 	private float despCoche2X;
 	private float despCoche2Y;
 
+	private float despCoche3X;
+	private float despCoche3Y;
+
 	// coliciones
 	private Rectangulo rectanguloCoche1; //coliciones
 	private Rectangulo rectanguloCoche2; //coliciones
+	private Rectangulo rectanguloCoche3; //coliciones
 
+	//sonidos
+	private SonidoSoundPool sonidoCar, explosion;
+
+	//cotroles
+
+	private int swColExplo =0;
+	private  int swCarrera=0;
 
 	Random rando=new Random();
 
@@ -40,6 +51,8 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 	public Renderiza(Context contexto) {
 		super(contexto);
 		this.contexto = contexto;
+		sonidoCar= new SonidoSoundPool(contexto,"scar2.ogg");
+		explosion=new SonidoSoundPool(contexto,"explosion.ogg");
 		this.setRenderer(this);
 		this.requestFocus();
 		this.setFocusableInTouchMode(true);
@@ -60,16 +73,28 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 		texturaCoche2 = new Textura(gl, contexto, "f2_orange.png");
 		texturaCoche2.setVertices(112, 50, 40, 50);
 
-		//coliciones
-
+		texturaCoche3 = new Textura(gl, contexto, "f1_yellow.png");
+		texturaCoche3.setVertices(112, 50, 40, 50);
 
 
 		despCarreteraY = 0;
 
 		desplCoche1x=0;
+
 		despCoche2X = 64;
 		despCoche2Y = 480;
-        
+
+		despCoche3X = 64;
+		despCoche3Y = 240;
+
+		//coliciones
+		rectanguloCoche1=new Rectangulo(112,50,40,50);
+		rectanguloCoche2=new Rectangulo(112+ despCoche2X,50+despCoche2Y,40,50);
+		rectanguloCoche3=new Rectangulo(112+ despCoche3X,50+despCoche3Y,40,50);
+
+		//sonidos
+		sonidoCar.repeat();
+
 		/* Habilita la textura */
 		gl.glEnable(GL10.GL_TEXTURE_2D);
 		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
@@ -96,6 +121,8 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 		gl.glTranslatef(desplCoche1x,0,0);
 		gl.glMatrixMode(GL10.GL_TEXTURE);
 		gl.glLoadIdentity();
+		rectanguloCoche1.x=112+desplCoche1x;
+
 		gl.glBindTexture(GL10.GL_TEXTURE_2D,
 				texturaCoche1.getCodigoTextura());
 		texturaCoche1.muestra(gl);
@@ -104,6 +131,10 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 	public void dibujaCoche2(GL10 gl){
 		gl.glMatrixMode(GL10.GL_MODELVIEW);
 		gl.glLoadIdentity();
+		//coliciones
+		rectanguloCoche2.x=112+despCoche2X;
+		rectanguloCoche2.y=50+despCoche2Y;
+
 		gl.glTranslatef(despCoche2X, despCoche2Y, 0);
 		gl.glMatrixMode(GL10.GL_TEXTURE);
 		gl.glLoadIdentity();
@@ -112,40 +143,98 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 		texturaCoche2.muestra(gl);
 	}
 
+	public void dibujaCoche3(GL10 gl){
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		//coliciones
+		rectanguloCoche3.x=112+despCoche3X;
+		rectanguloCoche3.y=50+despCoche3Y;
+
+		gl.glTranslatef(despCoche3X, despCoche3Y, 0);
+		gl.glMatrixMode(GL10.GL_TEXTURE);
+		gl.glLoadIdentity();
+		gl.glBindTexture(GL10.GL_TEXTURE_2D,
+				texturaCoche3.getCodigoTextura());
+		texturaCoche3.muestra(gl);
+	}
+
 	
 	@Override
 	public void onDrawFrame(GL10 gl) {
 
 		/* Inicializa el buffer de color y de profundidad */
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		dibujaCarretera(gl);
-		
+
 		dibujaCoche1(gl);
-		
+
 		dibujaCoche2(gl);
-
-		despCarreteraY = despCarreteraY - 0.01f;
-		if (despCarreteraY < -60)
-			despCarreteraY = 0;
+		dibujaCoche3(gl);
 
 
-		despCoche2Y = despCoche2Y - 5;
-		if (despCoche2Y < -60){
-			despCoche2Y = 480;
+		if (!seSobreponen(rectanguloCoche1, rectanguloCoche2)) {
+			if (!seSobreponen(rectanguloCoche1, rectanguloCoche3)) {
+				swColExplo = 0;
+				swCarrera = 0;
 
-			if(rando.nextInt(2)==0) {
-				despCoche2X = 0;
-				//desplCoche1x=64;
+				despCarreteraY = despCarreteraY - 0.01f;
+				if (despCarreteraY < -60)
+					despCarreteraY = 0;
+
+				//velocidad
+				despCoche2Y = despCoche2Y - 5;
+				despCoche3Y = despCoche3Y - 5;
+
+				//coche 2
+				if (despCoche2Y < -60) {
+					despCoche2Y = 480;
+
+					if (rando.nextInt(2) == 0) {
+						despCoche2X = 0;
+						despCoche3X = 64;
+
+						//desplCoche1x=64;
+					} else {
+						despCoche2X = 64;
+						despCoche3X = 0;
+
+						//desplCoche1x=0;
+					}
+
+				}
+				//coche 3
+				if (despCoche3Y < -60) {
+					despCoche3Y = 480;
+				}
+
+
+			} else {
+				swCarrera = 1;
+				if (swColExplo == 0) {
+					swColExplo = 1;
+					finDeJuegoSonido();
+				}
 			}
-				else {
-				despCoche2X = 64;
-				//desplCoche1x=0;
+
+		}else {
+			swCarrera = 1;
+			if (swColExplo == 0) {
+				swColExplo = 1;
+				finDeJuegoSonido();
 			}
 		}
-
 	}
-	
+
+public void movimientoCoches(float despCoche2X, float despCoche2Y){
+
+}
+
+	private void finDeJuegoSonido() {
+		explosion.play();
+		sonidoCar.stop();
+	}
+
 	@Override
 	public void onSurfaceChanged(GL10 gl, int w, int h) {
 	 
@@ -172,17 +261,23 @@ public class Renderiza extends GLSurfaceView implements GLSurfaceView.Renderer{
 	@Override
 	public boolean onTouchEvent(MotionEvent e){
 		if(e.getAction()==MotionEvent.ACTION_UP){
-			Toast toast1 = Toast.makeText(this.getContext(), "touch ok", Toast.LENGTH_SHORT);
-			toast1.show();
-
+			//explosion.play();
+			//Toast toast1 = Toast.makeText(this.getContext(), "touch ok", Toast.LENGTH_SHORT);
+			//toast1.show();
 			if(desplCoche1x==0)
 				desplCoche1x=64;
 			else
 				desplCoche1x=0;
 		}
+
+
 		return true;
 	}
 
+	public boolean seSobreponen(Rectangulo r1, Rectangulo r2){
+		return (r1.x < r2.x + r2.ancho && r2.x < r1.x + r1.ancho &&
+				r1.y < r2.y + r2.alto && r2.y < r1.y + r1.alto);
+	}
 
 
 
